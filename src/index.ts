b0a1f490e18da20b78a18b67624b5ea4569760bf
David Castaneda <dcast188@fiu.edu>
2021-01-02 11:50:10 -0500

    (feat) logs for deployment action

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,18 @@ const run = async () => {
   const file = core.getInput("file");
   const config = await resolveConfig(file);
 
+  core.info(`Using Krane instance ${url}`);
+  core.info(` Deployment configuration \n ${JSON.stringify(config, null, 2)}`);
+
   const client = new KraneClient(url, token);
+
+  core.info(`Saving ${config.name} configuration`);
   await client.saveDeployment(config);
+  core.info(`Configuration for ${config.name} saved succesfully`);
+
+  core.info(`Triggering new run for deployment ${config.name}`);
   await client.runDeployment(config.name);
+  core.info(`Deployment ${config.name} triggered succesfully`);
 };
 
 run().catch((error: Error) => core.setFailed(error.message));
