d137c39a86a148225fc030b81ad08a7ff6521068
David Castaneda <dcast188@fiu.edu>
2021-01-02 12:37:32 -0500

    (fix) change core.info --> console.log

--- src/config.ts
+++ src/config.ts
@@ -6,16 +6,5 @@ const readFile = promises.readFile;
 export const resolveConfig = async (path: string) => {
   const rawConfig = await readFile(path, "utf8");
   const config = JSON.parse(rawConfig) as Config;
-  applyConfigDefaults(config);
   return config;
-};
-
-const applyConfigDefaults = (config: Config) => {
-  if (config.tag == null) {
-    config.tag = "latest";
-  }
-
-  if (config.scale == null) {
-    config.scale = 1;
-  }
 };
--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,24 @@ const run = async () => {
   const url = core.getInput("url");
   const token = core.getInput("token");
   const file = core.getInput("file");
-  const config = await resolveConfig(file);
 
-  core.info(`Using Krane instance ${url}`);
-  core.info(` Deployment configuration \n ${JSON.stringify(config, null, 2)}`);
+  const config = await resolveConfig(file);
+  if (config.scale == null) {
+    config.scale = 1;
+  }
+
+  console.log(`Deploying to Krane instance ${url}`);
+  console.log(` Deployment configuration \n${JSON.stringify(config, null, 2)}`);
 
   const client = new KraneClient(url, token);
 
-  core.info(`Saving ${config.name} configuration`);
+  console.log(`Saving ${config.name} configuration`);
   await client.saveDeployment(config);
-  core.info(`Configuration for ${config.name} saved succesfully`);
+  console.log(`Configuration for ${config.name} saved succesfully`);
 
-  core.info(`Triggering new run for deployment ${config.name}`);
+  console.log(`Triggering new run for ${config.name}`);
   await client.runDeployment(config.name);
-  core.info(`Deployment ${config.name} triggered succesfully`);
+  console.log(`Deployment ${config.name} triggered succesfully`);
 };
 
 run().catch((error: Error) => core.setFailed(error.message));
