f86fae0ddaa257e7440146309b678db6a3fa78f6
David Castaneda <dcast188@fiu.edu>
2021-01-03 18:37:58 -0500

    (fix) output

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,16 @@ const run = async () => {
   const url = core.getInput("url");
   const token = core.getInput("token");
   const file = core.getInput("file");
-
   const config = await resolveConfig(file);
-  core.info(` Deployment configuration:\n${JSON.stringify(config, null, 2)}`);
 
   const client = new KraneClient(url, token);
-
-  core.startGroup(`Save ${config.name} configuration`);
-  core.info(`Saving ${config.name} configuration`);
+  core.startGroup(`Saving ${config.name} configuration`);
+  core.info(` Deployment configuration:\n${JSON.stringify(config, null, 2)}`);
   await client.saveDeployment(config);
   core.info(`Configuration for ${config.name} saved succesfully`);
   core.endGroup();
 
-  core.startGroup(`Trigger deployment ${config.name}`);
+  core.startGroup(`Triggering deployment ${config.name}`);
   core.info(`Triggering new run for ${config.name}`);
   await client.runDeployment(config.name);
   core.info(`Deployment ${config.name} triggered succesfully`);
