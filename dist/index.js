6fca4b2a169ccee020e1994dcee671443a854a37
David Castaneda <dcast188@fiu.edu>
2021-01-04 17:45:00 -0500

    (build)

--- dist/index.js
+++ dist/index.js
@@ -2,11 +2,97 @@ require('./sourcemap-register.js');module.exports =
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
+/***/ 9139:
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+"use strict";
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.KraneAction = void 0;
+const core = __importStar(__webpack_require__(2186));
+class KraneAction {
+    constructor(client, config) {
+        this.client = client;
+        this.config = config;
+    }
+    saveDeployment() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const deployment = this.config.name;
+            core.startGroup(`Saving configuration for deployment ${deployment}`);
+            core.info(`Configuration:\n${JSON.stringify(this.config, null, 2)}`);
+            yield this.client.saveDeployment(this.config);
+            core.info(`Configuration for deployment ${deployment} saved succesfully`);
+            core.endGroup();
+        });
+    }
+    runDeployment() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const deployment = this.config.name;
+            core.startGroup(`Triggering deployment ${deployment}`);
+            core.info(`Triggering new run for ${deployment}`);
+            yield this.client.runDeployment(deployment);
+            core.info(`Deployment ${deployment} triggered succesfully`);
+            core.endGroup();
+        });
+    }
+}
+exports.KraneAction = KraneAction;
+
+
+/***/ }),
+
 /***/ 88:
 /***/ (function(__unused_webpack_module, exports, __webpack_require__) {
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -18,13 +104,16 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.resolveConfig = void 0;
+const core = __importStar(__webpack_require__(2186));
 const fs_1 = __webpack_require__(5747);
 const readFile = fs_1.promises.readFile;
 const resolveConfig = (path) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
     const rawConfig = yield readFile(path, "utf8");
     const config = JSON.parse(rawConfig);
+    const tag = (_a = core.getInput("tag")) !== null && _a !== void 0 ? _a : "latest";
     if (config.tag == null) {
-        config.tag = "latest";
+        config.tag = tag;
     }
     if (config.scale == null) {
         config.scale = 1;
@@ -73,22 +162,16 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__webpack_require__(2186));
 const common_1 = __webpack_require__(1639);
 const config_1 = __webpack_require__(88);
+const action_1 = __webpack_require__(9139);
 const run = () => __awaiter(void 0, void 0, void 0, function* () {
     const endpoint = core.getInput("endpoint");
     const token = core.getInput("token");
     const file = core.getInput("file");
     const config = yield config_1.resolveConfig(file);
     const client = new common_1.KraneClient(endpoint, token);
-    core.startGroup(`Saving ${config.name} configuration`);
-    core.info(` Deployment configuration:\n${JSON.stringify(config, null, 2)}`);
-    yield client.saveDeployment(config);
-    core.info(`Configuration for ${config.name} saved succesfully`);
-    core.endGroup();
-    core.startGroup(`Triggering deployment ${config.name}`);
-    core.info(`Triggering new run for ${config.name}`);
-    yield client.runDeployment(config.name);
-    core.info(`Deployment ${config.name} triggered succesfully`);
-    core.endGroup();
+    const action = new action_1.KraneAction(client, config);
+    yield action.saveDeployment();
+    yield action.runDeployment();
 });
 run().catch((error) => core.setFailed(error.message));
 
